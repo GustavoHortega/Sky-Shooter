9e191612bf54c5fd3995d54769d295e2846362b7
Gustavo Hortega <gustavohortega80@gmail.com>
2026-07-28 18:42:14 -0400

    first commit

---
+++ code/Game.py
@@ -0,0 +1,23 @@
+import pygame
+
+from Menu import Menu
+
+
+class Game:
+
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(800, 600))
+
+
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+
+        #     # Checa eventos
+        #     for event in pygame.event.get():
+        #         if event.type == pygame.QUIT:
+        #             pygame.quit() # Fecha Janela
+        #             exit() # Encerra o Pygame
