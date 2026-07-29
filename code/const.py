92aface5c09c9c80f51dd5fff7b8ef28810a55d4
Gustavo Hortega <gustavohortega80@gmail.com>
2026-07-28 20:10:59 -0400

    feat: Adiciona arquivo de constantes.

--- code/const.py
+++ code/const.py
@@ -1,2 +1,15 @@
+# C
+COLOR_RED = (215, 38, 56)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTIONS = ('NEW GAME 1P',
+                'NEW GAME 2P - COOPERATIVE',
+                'NEW GAME 2P - COMPETITIVE',
+                'SCORE',
+                'EXIT')
+
+
+# W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
